0521b3cdc77b83afcfac46676261f9120967950e
harshPPatel <36562266+harshPPatel@users.noreply.github.com>
2018-10-22 22:50:42 -0500

    Smooth Scrolling Added To The Project
    It was a Fun!!!

--- source/js/app.js
+++ source/js/app.js
@@ -9,4 +9,16 @@ $(document).ready(function() {
         $("nav").removeClass("sticky");
     }
   });
+
+  $('.js--scroll-to-plans').click(function () {
+    $('html, body').animate({scrollTop: $('.js--plans').offset().top}, 1000);
+  });
+
+  $('.js--scroll-to-start').click(function () {
+    $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+  });
+
+  $('.js--logo').click(function () {
+    $('html, body').animate({scrollTop: $('header').offset().top}, 1000);
+  });
 });
