25cc495c82306d9bc92ee27fd553c1a02e759fc8
harshPPatel <36562266+harshPPatel@users.noreply.github.com>
2019-07-22 20:31:09 -0500

    Now Configuration added!

--- source/js/app.js
+++ source/js/app.js
@@ -1,36 +1,38 @@
-// Fade up navbar on scroll
+// Document Ready Event function
 $(document).ready(function() {
-
   $(window).scroll(function() {
     var scroll = $(window).scrollTop();
     if (scroll >= 600) {
-        $("nav").addClass("sticky");
-        $("#toggle").addClass("sticky-toggle");
+      $('nav').addClass('sticky');
+      $('#toggle').addClass('sticky-toggle');
     } else {
-        $("nav").removeClass("sticky");
-        $("#toggle").removeClass("sticky-toggle");
+      $('nav').removeClass('sticky');
+      $('#toggle').removeClass('sticky-toggle');
     }
   });
 
-  $('.js--mobile-navlinks').click(function () {
+  $('.js--mobile-navlinks').click(function() {
     $('#resize').removeClass('active');
     $('#toggle').removeClass('on');
   });
 
-  $('.js--scroll-to-plans').click(function () {
-    $('html, body').animate({scrollTop: $('.js--plans').offset().top}, 1000);
+  $('.js--scroll-to-plans').click(function() {
+    $('html, body').animate({ scrollTop: $('.js--plans').offset().top }, 1000);
   });
 
-  $('.js--scroll-to-start').click(function () {
-    $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+  $('.js--scroll-to-start').click(function() {
+    $('html, body').animate(
+      { scrollTop: $('.js--section-features').offset().top },
+      1000
+    );
   });
 
-  $('.js--logo').click(function () {
-    $('html, body').animate({scrollTop: $('header').offset().top}, 1000);
+  $('.js--logo').click(function() {
+    $('html, body').animate({ scrollTop: $('header').offset().top }, 1000);
   });
 
-  $("#toggle").click(function(){
+  $('#toggle').click(function() {
     $(this).toggleClass('on');
-    $("#resize").toggleClass("active");
+    $('#resize').toggleClass('active');
   });
 });
